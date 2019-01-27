af3f0ec2fb455e7eb2f1af08c1389e65384331b5
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-01-27 08:42:58 +0530

    Create Strcount.c

---
+++ Strcount.c
@@ -0,0 +1,18 @@
+int main()
+{
+	int i,b,j=0;
+	char a[100];
+	gets(a);
+	for(i=0;a[i]!='\0';i++);
+	b=i;
+	//printf("%d",b);
+	for(i=0;i<b;i++)
+	{ 
+		if(a[i]==' ')
+		{}
+		else
+		 j++;
+	}
+	printf("%d",j);
+	return 0;
+}
